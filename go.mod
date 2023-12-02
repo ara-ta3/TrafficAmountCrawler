67a114fce89ca653f7ef6afbab3a5a3e200cf6c2
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2023-12-02 18:16:29 +0900

    Use viper

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/playwright-community/playwright-go v0.3900.1 // indirect
+	github.com/spf13/viper v1.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	c, e := Load()
+	fmt.Printf("%+v, %+v", c, e)
 }
+
+
+func Load() (config *EnvConfigs, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+	return
+}
+
+type EnvConfigs struct {
+	NihonTsushinID   string `mapstructure:"NIHON_TSUSHIN_ID"`
+	NihonTsushinPass string `mapstructure:"NIHON_TSUSHIN_PASS"`
+}
+
